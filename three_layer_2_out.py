ea7ef4a3aa369194b529f2554b7829bea995a2a1
rlkelly <robert.l.kelly3@gmail.com>
2017-02-07 16:03:43 -0500

    update

--- three_layer_2_out.py
+++ three_layer_2_out.py
@@ -2,38 +2,42 @@
 
 # We set the mean of these weights around 0
 # These are the initial weights for your two layers
-first_weights = 2*np.random.random((5,4)) - 1
-second_weights = 2*np.random.random((4,3)) - 1
-third_weights = 2*np.random.random((3,2)) - 1
+first_weights = 2 * np.random.random((5, 4)) - 1
+second_weights = 2 * np.random.random((4, 3)) - 1
+third_weights = 2 * np.random.random((3, 2)) - 1
 
 # Lists to track the error rate
 error_list = []
 graph_errors = []
 
 # Activation Function
+
+
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 # Derivitive of Activation Function with Respect to Error
+
+
 def backpropogation(x):
-    return x*(1-x)
+    return x * (1 - x)
+
 
 def secret_formula(lst):
-    return ((lst[0]*lst[3]+lst[4])/lst[2] > 1.3).conjugate(), (sum(lst) < 2).conjugate()
+    return ((lst[0] * lst[3] + lst[4]) / lst[2] > 1.3).conjugate(), (sum(lst) < 2).conjugate()
 
 # Take 100,000 batches of 100
 for batch in xrange(500000):
     temp = [[random.random() for _ in range(5)] for _ in range(1)]
-    
-    X = np.array(temp)
-    y = np.array( [secret_formula(row) for row in X] )
 
-    
+    X = np.array(temp)
+    y = np.array([secret_formula(row) for row in X])
+
     # layer_0 is your input matrix
     layer_0 = X
     # layer_1 is your first hidden layer
     layer_1 = sigmoid(np.dot(layer_0, first_weights))
-    
+
     # layer_2 is your second hidden layer
     layer_2 = sigmoid(np.dot(layer_1, second_weights))
 
@@ -44,7 +48,7 @@ for batch in xrange(500000):
     output_error = y - layer_3
 
     # Take product of slope of sigmoid and output error
-    layer_3_delta = (output_error*backpropogation(layer_3))
+    layer_3_delta = (output_error * backpropogation(layer_3))
 
     # Find error of Layer 2 and get derivitive of layer 2 with respect to error
     layer_2_error = layer_3_delta.dot(third_weights.T)
@@ -56,9 +60,9 @@ for batch in xrange(500000):
 
     # Use an alpha level of .01 to decrease rate of descent
     # This helps to avoid local minimums!
-    third_weights += layer_2.T.dot(layer_3_delta)*.01
-    second_weights += layer_1.T.dot(layer_2_delta)*.01
-    first_weights += layer_0.T.dot(layer_1_delta)*.01
+    third_weights += layer_2.T.dot(layer_3_delta) * .01
+    second_weights += layer_1.T.dot(layer_2_delta) * .01
+    first_weights += layer_0.T.dot(layer_1_delta) * .01
 
     # Calculate Current Error
     error = np.mean(np.abs(output_error))
@@ -66,8 +70,9 @@ for batch in xrange(500000):
     error_list.append(error)
 
     if (batch % 10000) == 0 and batch > 0:
-        mean_error = (sum(error_list)*1.)/10000
+        mean_error = (sum(error_list) * 1.) / 10000
         graph_errors.append(mean_error)
         error_list = []
 
-        print("Error at iteration {BATCH}:\t \t {ERROR}".format(BATCH=batch, ERROR=mean_error))
+        print("Error at iteration {BATCH}:\t \t {ERROR}".format(
+            BATCH=batch, ERROR=mean_error))
--- twolayer.py
+++ twolayer.py
@@ -1,7 +1,7 @@
 # We set the mean of these weights around 0
 # These are the initial weights for your two layers
-first_weights = 2*np.random.random((5,20)) - 1
-second_weights = 2*np.random.random((20,1)) - 1
+first_weights = 2 * np.random.random((5, 20)) - 1
+second_weights = 2 * np.random.random((20, 1)) - 1
 
 # second_weights = 2*np.random.random((20,10)) - 1
 # third_weights = 2*np.random.random((10,1)) - 1
@@ -13,30 +13,34 @@ error_list = []
 graph_errors = []
 
 # Activation Function
+
+
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 # Derivitive of Activation Function with respect to Error
+
+
 def backpropogation(x):
-    return x*(1-x)
+    return x * (1 - x)
+
 
 def secret_formula(lst):
-    return (lst[0]*lst[3]+lst[4])/lst[2]
+    return (lst[0] * lst[3] + lst[4]) / lst[2]
 
 # Take 100,000 batches of 100
 for batch in xrange(500000):
     temp = [[random.random() for _ in range(5)] for _ in range(100)]
-    
+
     X = np.array(temp)
     #     y = np.array( [[(row.sum() >= 2.5).conjugate()] for row in X] )
-    y = np.array( [[(secret_formula(row) > 1.1).conjugate()] for row in X] )
+    y = np.array([[(secret_formula(row) > 1.1).conjugate()] for row in X])
 
-    
     # layer_0 is your input matrix
     layer_0 = X
     # layer_1 is your first hidden layer
     layer_1 = sigmoid(np.dot(layer_0, first_weights))
-    
+
     # layer_2 is your second hidden layer
     layer_2 = sigmoid(np.dot(layer_1, second_weights))
 
@@ -62,8 +66,8 @@ for batch in xrange(500000):
 
     # Use an alpha level of .01 to decrease rate of descent
     # This helps to avoid local minimums!
-    second_weights += layer_1.T.dot(layer_2_delta)*.01
-    first_weights += layer_0.T.dot(layer_1_delta)*.01
+    second_weights += layer_1.T.dot(layer_2_delta) * .01
+    first_weights += layer_0.T.dot(layer_1_delta) * .01
 
     # Calculate Current Error
     error = np.mean(np.abs(layer_2_error))
@@ -71,8 +75,8 @@ for batch in xrange(500000):
     error_list.append(error)
 
     if (batch % 10000) == 0 and batch > 0:
-        mean_error = (sum(error_list)*1.)/10000
+        mean_error = (sum(error_list) * 1.) / 10000
         graph_errors.append(mean_error)
         error_list = []
 
-        print "Error at iteration %s:\t \t %s" %(batch, mean_error)
+        print "Error at iteration %s:\t \t %s" % (batch, mean_error)
